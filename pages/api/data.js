06715d27e15bb64337904348230f69c046c97671
Bertrand Goupil <bertrand.goupil@allianz-assistance.com>
2020-10-06 09:35:50 +0200

    test SWR

---
+++ pages/api/data.js
@@ -0,0 +1,15 @@
+import fetch from 'isomorphic-unfetch'
+  let elasticURL = process.env.REACT_APP_ELK_URL;
+  export default (req, res) => {
+    
+      // a slow endpoint for getting repo data
+      fetch(elasticURL)
+        .then(resp => resp.json())
+        .then(data => {
+          setTimeout(() => {
+            res.json(data)
+          }, 2000)
+        })
+      
+      return
+  }
---
+++ pages/tools/testswr.js
@@ -0,0 +1,18 @@
+import fetch from "../../components/shared/lib/fetch";
+import useSWR from 'swr';
+
+const TestSWR = () => {
+    const { data } = useSWR('/api/data', fetch);
+    if (!data) return <h1>loading...</h1>
+    return (
+        <div>
+          <h2>Todo List</h2>
+       
+          <ul>
+            {data.hits.hits.map(item => <li key={item._source.id_str}>{item._source.full_text}</li>)}
+          </ul>
+          
+        </div>
+      )
+    }
+export default TestSWR;
