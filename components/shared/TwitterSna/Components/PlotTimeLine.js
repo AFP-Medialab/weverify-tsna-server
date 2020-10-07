47269b45c2108b598e3a9106be5750e98739edeb
anatoleroulet <anatole.roulet@epita.fr>
2020-10-07 15:32:26 +0200

    set histo

--- components/shared/TwitterSna/Components/PlotTimeLine.js
+++ components/shared/TwitterSna/Components/PlotTimeLine.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import plotly from 'plotly.js-dist';
 import createPlotComponent from 'react-plotly.js/factory';
-import useLoadLanguage from "../../languages/languages"
+import useLoadLanguage from "../../hooks/useLoadLanguage";
+import {displayTweets} from "../lib/displayTweets";
 
 const Plot = createPlotComponent(plotly);
 
@@ -49,7 +50,6 @@ export default function PlotTimeLine(props){
 
     const onHistogramClick = (data) => {
         
-        console.log("onhisto click : " + state.result.tweets);
         if (state.result.tweets !== undefined) {
             
             let selectedPoints = data.points;
@@ -57,73 +57,11 @@ export default function PlotTimeLine(props){
                 let tweetDate = new Date(tweet._source.datetimestamp * 1000);
                 return filterTweetsForTimeLine(tweetDate, selectedPoints);
             });
-            setHistoTweets(displayTweets(filteredTweets));
+            setHistoTweets(displayTweets(filteredTweets, keyword));
         }
     }
 
-    const displayTweets = (filteredTweets, sortedColumn) => {
-
-        let columns = [];
-        if (sortedColumn === "nbLikes") {
-            columns = [
-                { title: keyword('twittersna_result_date'), field: 'date'},
-                { title: keyword('twittersna_result_username'), field: 'screen_name'},
-                { title: keyword('twittersna_result_tweet'), field: 'tweet', render: getTweetWithClickableLink },
-                { title: keyword('twittersna_result_like_nb'), field: "nbLikes", defaultSort: "desc" },
-                { title: keyword('twittersna_result_retweet_nb'), field: 'retweetNb'}
-            ];
-        } else if (sortedColumn === "retweetNb") {
-            columns = [
-                { title: keyword('twittersna_result_date'), field: 'date'},
-                { title: keyword('twittersna_result_username'), field: 'screen_name'},
-                { title: keyword('twittersna_result_tweet'), field: 'tweet', render: getTweetWithClickableLink },
-                { title: keyword('twittersna_result_like_nb'), field: "nbLikes"},
-                { title: keyword('twittersna_result_retweet_nb'), field: 'retweetNb', defaultSort: "desc" }
-            ];
-        } else {
-            columns = [
-                { title: keyword('twittersna_result_date'), field: 'date', defaultSort: "asc" },
-                { title: keyword('twittersna_result_username'), field: 'screen_name'},
-                { title: keyword('twittersna_result_tweet'), field: 'tweet', render: getTweetWithClickableLink },
-                { title: keyword('twittersna_result_like_nb'), field: "nbLikes"},
-                { title: keyword('twittersna_result_retweet_nb'), field: 'retweetNb'}
-            ];
-        }
-
-        let csvArr = keyword("twittersna_result_date") + ',' 
-                    + keyword("twittersna_result_username") + ',' 
-                    + keyword("twittersna_result_tweet") + ',' 
-                    + keyword('twittersna_result_like_nb') + ',' 
-                    + keyword("twittersna_result_retweet_nb") + ',' 
-                    + keyword("elastic_url") +'\n';
-
-        let resData = [];
-        filteredTweets.forEach(tweetObj => {
-            const date = new Date(tweetObj._source.datetimestamp * 1000);
-            resData.push(
-                {
-                    date: date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes(),
-                    screen_name: <a href={"https://twitter.com/" + tweetObj._source.screen_name} target="_blank" rel="noopener noreferrer">{tweetObj._source.screen_name}</a>,
-                    tweet: tweetObj._source.full_text,
-                    nbLikes: tweetObj._source.favorite_count,
-                    retweetNb: tweetObj._source.retweet_count,
-                    link: "https://twitter.com/" + tweetObj._source.screen_name + "/status/" + tweetObj._source.id_str
-                }
-            );
-            csvArr += date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + '_' + date.getHours() + 'h' + date.getMinutes() + ',' +
-                        tweetObj._source.screen_name + ',"' 
-                        + tweetObj._source.full_text + '",' 
-                        + tweetObj._source.favorite_count + ','
-                        + tweetObj._source.retweet_count + ',' 
-                        + "https://twitter.com/" + tweetObj._source.screen_name + "/status/" + tweetObj._source.id_str + '\n';
-        });
-
-        return {
-            data: resData,
-            columns: columns,
-            csvArr: csvArr,
-        };
-    }
+    
 
     return (
         <Plot useResizeHandler
