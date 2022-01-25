731676dc8a6553b24b65185c67619d79ef7a73cb
Bertrand Goupil <bertrand.goupil@afp.com>
2022-01-25 12:35:51 +0100

    Add not and user in search context

--- components/SNA/TwitterSna/Hooks/tsnaUtils.js
+++ components/SNA/TwitterSna/Hooks/tsnaUtils.js
@@ -10,6 +10,12 @@ export function widgetTitle(request){
     title += request.keywordList.length === 0 ? "" :" - " 
     title += request.keywordAnyList.join(" or ") 
     title +=  request.keywordAnyList.length === 0 ? "" :" - " 
+    title += request.bannedWords.length === 0 ? "" : " not "
+    title += request.bannedWords.join(", ")
+    title += request.bannedWords.length === 0 ? "" : " - "
+    title += request.userList.length === 0 ? "" : "user : "
+    title += request.userList.join(", ")
+    title += "<br />"
     title += request["from"] + " - " + request["until"]
     return title;
 }
@@ -19,6 +25,8 @@ export function widgetPieTitle(request){
     let title = request.keywordList.join(", ").replace(/#/g, "")
     title += request.keywordList.length === 0 ? "" :" - " 
     title += request.keywordAnyList.join(" or ").replace(/#/g, "")
+    title += request.bannedWords.length === 0 ? "" : " not "
+    title += request.bannedWords.join(", ").replace(/#/g, "")
     return title;
 }
 
