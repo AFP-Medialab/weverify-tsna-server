b72041a8b7d298ab37b08436aded6cdd0ebdf3d0
Bertrand Goupil <bertrand.goupil@afp.com>
2022-01-26 18:43:41 +0100

    shell

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ ENV_CONTENT=$1
 while IFS= read -r line; do
     key="${line=*}"
     value="${line#*=}"
+    echo "key = $key"
+    echo "value = $value"
     #eval "$key"="$value" # For ash, dash, sh.
     declare "$key"="$value" # For bash, other shells.
 done < $1 
