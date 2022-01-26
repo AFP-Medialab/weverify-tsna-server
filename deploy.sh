7b47d8dfaae83531d55049c85792c128ae80ecbc
Bertrand Goupil <bertrand.goupil@afp.com>
2022-01-26 18:42:22 +0100

    shell

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 ENV_CONTENT=$1
 while IFS= read -r line; do
     key="${line=*}"
-    value="${line*=}"
-    eval "$key"="$value" # For ash, dash, sh.
-    # declare "$key"="$value" # For bash, other shells.
+    value="${line#*=}"
+    #eval "$key"="$value" # For ash, dash, sh.
+    declare "$key"="$value" # For bash, other shells.
 done < $1 
 echo "username $SSH_USERNAME"
 echo "hostname $SSH_HOSTNAME"
