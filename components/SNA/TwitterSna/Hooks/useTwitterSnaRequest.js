b96710e92d34e70470e3b32b100c8b0d5c7a05e1
Bertrand Goupil <bertrand.goupil@afp.com>
2022-06-01 16:10:33 +0200

    remove esURL to gexf body bug fix import missing

--- components/SNA/TwitterSna/Hooks/useTwitterSnaRequest.js
+++ components/SNA/TwitterSna/Hooks/useTwitterSnaRequest.js
@@ -24,6 +24,7 @@ import {
   getCloudTweets,
   getESQuery4Gexf,
 } from "./call-elastic";
+import {userLogoutAction} from "../../../../redux/actions/authentificationActions"
 import { getJsonDataForTimeLineChart } from "./timelineTW";
 import { createTimeLineChart } from "../../Hooks/timeline";
 import { createPieCharts, getJsonDataForPieCharts } from "./pieCharts";
