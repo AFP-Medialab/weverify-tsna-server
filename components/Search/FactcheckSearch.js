3216ba5768f9eee8c09d6d05c95419749e8ff336
Bertrand Goupil <bertrand.goupil@afp.com>
2024-03-12 10:12:13 +0100

    fix script

--- components/Search/FactcheckSearch.js
+++ components/Search/FactcheckSearch.js
@@ -17,8 +17,8 @@ const FactcheckSearch = () => {
 
   useEffect(() => {
     const script = document.createElement('script');
-
-    script.src = "https://cse.google.com/cse?cx=" + keyword("factcheck_search_engines");
+    console.log("key", keyword("factcheck_search_engines"))
+    script.src = "https://cse.google.com/cse.js?cx=" + keyword("factcheck_search_engines");
     script.async = true;
 
     if (script.src !== "https://cse.google.com/cse.js?cx=") {
