ecd8b30079aa24797ca9063ca6f1a2479b5583d1
robfrks <robin.frks@gmail.com>
2024-06-05 10:47:17 +0200

    added i18n files

---
+++ components/i18n.jsx
@@ -0,0 +1,44 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HTTPBackend from "i18next-http-backend";
+import ChainedBackend from "i18next-chained-backend";
+//import resourcesToBackend from "i18next-resources-to-backend";
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(ChainedBackend)
+  .init({
+    //debug: true,
+    ns: ["components/PopUp"],
+    defaultNS: "components/PopUp",
+    fallbackLng: "en",
+    saveMissing: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    load: "languageOnly",
+    backend: {
+      backends: [
+        HTTPBackend,
+        //resourcesToBackend((lng, ns) => import(`./LocaLDictionary/locales/${lng}/${ns}.json`))
+        HTTPBackend,
+      ],
+
+      backendOptions: [
+        {
+          loadPath: `${process.env.REACT_APP_I18N_CONTENT}/dictionaries/{{ns}}.tsv?lang={{lng}}&tag=${process.env.REACT_APP_TRANSLATION_TAG}`,
+          crossDomain: true,
+          requestOptions: {
+            // used for fetch, can also be a function (payload) => ({ method: 'GET' })
+            mode: "cors",
+            credentials: "same-origin",
+          },
+        },
+        {
+          loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+      ],
+    },
+  });
